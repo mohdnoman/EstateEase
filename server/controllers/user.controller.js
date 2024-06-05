f10f77f73ce188cc80bce3f159ba539cb1aa8c42
mohd noman <mohdnoman2751@gmail.com>
2024-06-05 20:59:07 +0530

    Add contact property owner funtionality to the listing page.

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -77,9 +77,26 @@ const getUserListings = async (req, res, next) => {
     }
 }
 
+const getUser = async (req, res, next) => {
+    try {
+
+        const user = await User.findById(req.params.id)
+
+        if (!user) {
+            return next(errorHandler(404, "user not found"));
+        }
+
+        const { password: pass, ...rest } = user._doc
+
+        res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
 
 export {
     updateUserInfo,
     deleteUser,
-    getUserListings
+    getUserListings,
+    getUser
 };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { updateUserInfo, deleteUser,getUserListings } from "../controllers/user.controller.js"
+import { updateUserInfo, deleteUser, getUserListings, getUser } from "../controllers/user.controller.js"
 import { VerifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,5 @@ const router = express.Router();
 router.post("/update/:id",VerifyUser, updateUserInfo)
 router.delete("/delete/:id",VerifyUser, deleteUser)
 router.get("/listings/:id", VerifyUser, getUserListings)
-
+router.get("/:id", VerifyUser, getUser)
 export default router
