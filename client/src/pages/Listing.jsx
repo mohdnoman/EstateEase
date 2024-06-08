beb11e3cecd09710029a2d8ca5e789934ae51f37
mohd noman <mohdnoman2751@gmail.com>
2024-06-09 01:31:51 +0530

    Create listings card component

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -90,8 +90,8 @@ const Listing = () => {
             <p className="text-2xl font-semibold">
               {listing.name} - ${" "}
               {listing.offer
-                ? listing.discountPrice.toLocaleString("en-US")
-                : listing.regularPrice.toLocaleString("en-US")}
+                ? listing.discountedPrice?.toLocaleString("en-US")
+                : listing.regularPrice?.toLocaleString("en-US")}
               {listing.type === "rent" && " / month"}
             </p>
             <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
@@ -104,7 +104,7 @@ const Listing = () => {
               </p>
               {listing.offer && (
                 <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
-                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                  ${+listing.regularPrice - +listing.discountedPrice} OFF
                 </p>
               )}
             </div>
